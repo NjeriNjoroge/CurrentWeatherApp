341e071ae635fc2dad628e0d4489c0b393ff6016
NjeriNjoroge <grace.njoroge@incentro.com>
2020-10-13 12:26:32 +0300

    initial commit assets added

---
+++ WeatherApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Grace Njoroge on 13/10/2020.
+//  Copyright © 2020 Grace. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.rootViewController = MainViewController()
+    return true
+  }
+
+}
+
---
+++ WeatherApp/Controllers/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  WeatherApp
+//
+//  Created by Grace Njoroge on 13/10/2020.
+//  Copyright © 2020 Grace. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
